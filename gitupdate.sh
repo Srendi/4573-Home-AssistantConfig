c8dbb96a1c01db68dbe22b036e23b19e5a0fa8b7
Srendi <srendi@gmail.com>
2018-04-29 03:10:02 +1000

    added gitupdate.sh

---
+++ gitupdate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git add .
+git status
+if [[ -z $1 ]]; then
+	echo -n "Enter the Description for the Change: " [Minor Update]
+	read CHANGE_MSG
+	git commit -m "${CHANGE_MSG}"
+else
+        git commit -m "$1"
+fi
+git push origin master
+
+exit
